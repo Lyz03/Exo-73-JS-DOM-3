19d4a49a53d6cfcd9839d8f8d38230964ab74673
Lyz03 <lizoe@netcourrier.com>
2021-11-18 09:42:31 +0100

    part 1

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,5 +1,10 @@
 let avatarEls = document.getElementsByTagName("img");
 
+for (let i = 0; i < avatarEls.length; i++) {
+    avatarEls[i].style.height = '50px'
+    avatarEls[i].title = 'info-bulle'
+}
+
 
 
 
