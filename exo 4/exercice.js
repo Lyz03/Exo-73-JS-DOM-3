451677a6572f417ac41d674f42ce9e82bfb1ef89
Lyz03 <lizoe@netcourrier.com>
2021-11-18 09:59:09 +0100

    part 4

--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,4 +1,24 @@
+let div1 = document.createElement('div');
+div1.className = 'planet';
+div1.style.backgroundColor = 'blue';
 
+let div2 = document.createElement('div');
+div2.className = 'moon';
+
+let div3 = document.createElement('div');
+div3.className = 'planet';
+div3.style.backgroundColor = 'blue';
+div3.style.margin = '60px 60px 20px 50px'
+
+let div4 = document.createElement('div');
+div4.className = 'planet';
+div4.style.backgroundColor = 'blue';
+div4.style.margin = '100px 60px 20px 200px'
+
+document.body.appendChild(div1);
+document.body.appendChild(div2);
+document.body.appendChild(div3);
+document.body.appendChild(div4);
 
 
 
