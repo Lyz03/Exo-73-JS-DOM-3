18bc2a25fed19e4f656537950535489664c1fc7b
Lyz03 <lizoe@netcourrier.com>
2021-11-18 09:47:18 +0100

    part 2

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -2,8 +2,13 @@ let colorEl = document.getElementById("trendy-color");
 let handEl  = document.getElementById("trendy-handwriting");
 let frameEl = document.getElementById("trendy-frame");
 
+colorEl.innerText = 'purple';
+colorEl.style.color = 'purple';
 
+handEl.innerText = 'favorite celebrity';
+handEl.style.fontFamily = 'sans-serif'
 
+frameEl.style.border = '2px solid gray';
 
 
 
